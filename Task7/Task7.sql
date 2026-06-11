2c6fb3e31affa536580429a8f6f222f11dc9a380
Ganga48 <gr1656623@gmail.com>
2026-06-11 09:45:32 +0530

    Add files via upload

---
+++ Task7/Task7.sql
@@ -0,0 +1,38 @@
+USE resturant;
+
+CREATE TABLE customers(
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(50)
+);
+
+CREATE TABLE orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT
+);
+
+INSERT INTO customer VALUES
+(1,'vinod'),
+(2,'praveen'),
+(3,'sanjay');
+
+INSERT INTO orders VALUES
+(101,1),
+(102,2),
+(103,4),
+(104,1);
+
+-- INNER JOIN
+SELECT c.CustomerID,
+       c.CustomerName,
+       o.OrderID
+FROM customer c
+INNER JOIN orders o
+ON c.CustomerID = o.CustomerID;
+
+-- LEFT JOIN
+SELECT c.CustomerID,
+       c.CustomerName,
+       o.OrderID
+FROM customer c
+LEFT JOIN orders o
+ON c.CustomerID = o.CustomerID;
